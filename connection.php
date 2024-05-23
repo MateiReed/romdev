19fea53a0a3b54ae62b7d856a8dd8bc51d5fe448
Matei <116720757+MateiReed@users.noreply.github.com>
2024-05-23 22:10:17 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -3,12 +3,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = isset($_POST['username']) ? $_POST['username'] : '';
     $password = isset($_POST['password']) ? $_POST['password'] : '';
 
-
-
-// Crearea conexiunii
 $conn = new mysqli('localhost', 'admin', 'test', "romdev");
 
-// Verificăm conexiunea
 if ($conn->connect_error) {
     die("Conexiunea la baza de date a eșuat: " . $conn->connect_error);
 }
